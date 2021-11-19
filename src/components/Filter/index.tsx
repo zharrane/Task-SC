403a144e5efb80e2681b12b383ba04436bbeeb33
zharrane <zakariaharrane2@gmail.com>
2021-11-19 22:58:05 +0100

    working on home screen

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -4,12 +4,13 @@ import DropDown from "./DropDown"
 
 interface FilterProps {
   title: string
+  className?: string
 }
 interface CategoryType {
   id: string
   name: string
 }
-const Filter: React.FC<FilterProps> = ({ title }) => {
+const Filter: React.FC<FilterProps> = ({ title, className }) => {
   const [expandFilter, setExpandFiler] = useState<boolean>(false)
   const [rangeValue, setRangeValue] = useState<number>(10)
   const [minRangeValue, setMinValue] = useState<number>(0)
@@ -53,7 +54,7 @@ const Filter: React.FC<FilterProps> = ({ title }) => {
   }
 
   return (
-    <div className={`bg-gray-200 p-9`}>
+    <div className={clsx(className, `bg-gray-200 p-9 `)}>
       <div className={`flex justify-between items-center pr-10`}>
         <h1 className={`capitalize text-gray-700`}>{title}</h1>
         <DropDown
@@ -62,65 +63,67 @@ const Filter: React.FC<FilterProps> = ({ title }) => {
           className="lg:hidden"
         />
       </div>
-      {expandFilter && (
+
+      <div
+        className={clsx(
+          `container mt-8  lg:h-full lg:block  `,
+          expandFilter ? " " : "hidden "
+        )}
+      >
         <div
-          className={clsx(`container mt-8  `, expandFilter ? "h-52 " : "h-0")}
+          className={`flex justify-between container mx-auto overflow-hidden lg:flex-col lg:gap-8  lg:justify-start`}
         >
-          <div
-            className={`flex  justify-between container mx-auto overflow-hidden `}
-          >
-            <div className={`  flex flex-col gap-4 `}>
-              <span>Arrange</span>
-              <select className={`bg-secondary-500 text-gray-600`}>
-                <option>Popular</option>
-                <option>Latest</option>
-                <option>Hot bids</option>
-              </select>
-            </div>
-            <div className={`flex flex-col gap-4 `}>
-              <span>Minimum Bid</span>
-              <div className="flex gap-2 mr-5 items-end">
-                <span>{minRangeValue}</span>
-                <div className="flex flex-col">
-                  <input
-                    type="range"
-                    id="price-range"
-                    name="Price"
-                    min={minRangeValue}
-                    max={maxRangeValue}
-                    value={rangeValue}
-                    step="1"
-                    className={`rounded-lg  appearance-none bg-secondary-500 h-4 w-128`}
-                    onChange={handleOnChange}
-                  />
-                </div>
-                <span className="w-8">{rangeValue}$</span>
-              </div>
-            </div>
+          <div className={`  flex flex-col  gap-4 `}>
+            <span>Arrange</span>
+            <select className={`bg-secondary-500 text-gray-600`}>
+              <option>Popular</option>
+              <option>Latest</option>
+              <option>Hot bids</option>
+            </select>
           </div>
-          <div className="   mt-8">
-            <div>
-              <span>Category</span>
-              <div className="grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-1 mt-4">
-                {categories.map((category) => {
-                  return (
-                    <div key={category.id} className="flex gap-4 ">
-                      <input
-                        type="checkbox"
-                        id={category.name}
-                        value={category.name}
-                      />
-                      <label htmlFor={category.name} className={"capitalize"}>
-                        {category.name}
-                      </label>
-                    </div>
-                  )
-                })}
+          <div className={`flex flex-col gap-4 `}>
+            <span>Minimum Bid</span>
+            <div className="flex gap-2 mr-5 items-end">
+              <span>{minRangeValue}</span>
+              <div className="flex flex-col">
+                <input
+                  type="range"
+                  id="price-range"
+                  name="Price"
+                  min={minRangeValue}
+                  max={maxRangeValue}
+                  value={rangeValue}
+                  step="1"
+                  className={`rounded-lg  appearance-none bg-secondary-500 h-4 w-128`}
+                  onChange={handleOnChange}
+                />
               </div>
+              <span className="w-8">{rangeValue}$</span>
             </div>
           </div>
         </div>
-      )}
+        <div className="   mt-8">
+          <div>
+            <span>Category</span>
+            <div className="grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-1 mt-4">
+              {categories.map((category) => {
+                return (
+                  <div key={category.id} className="flex gap-4 ">
+                    <input
+                      type="checkbox"
+                      id={category.name}
+                      value={category.name}
+                    />
+                    <label htmlFor={category.name} className={"capitalize"}>
+                      {category.name}
+                    </label>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,18 @@ import Filter from "../components/Filter"
 
 const HomePage = () => {
   return (
-    <div>
-      <div>
-        <Filter title="filter" />
+    <div className="grid lg:grid-cols-5 lg:min-h-screen lg:overflow-hidden ">
+      <Filter title="filters" className={`lg:col-span-1 lg:pt-28`} />
+      <div className="grid grid-cols-1 lg:col-span-4 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-scroll overflow-x-hidden lg:min-h-screen">
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
+        <h1 className="bg-gray-400">Test Test Test</h1>
       </div>
     </div>
   )
