bd648fdbf0c23be441e8d58af7b7926ed552edfb
zharrane <zakariaharrane2@gmail.com>
2021-11-22 16:12:20 +0100

    purge style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 
   darkMode: false,
   theme: {
