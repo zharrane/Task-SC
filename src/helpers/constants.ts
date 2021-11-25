142c939ffeba3a20ae43d55cafeb6c2867692519
zharrane <zakariaharrane2@gmail.com>
2021-11-25 22:34:50 +0100

    change back point

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "https://task-sc-back-end.herokuapp.com/api"
+// const BASE_URL = "https://task-sc-back-end.herokuapp.com/api"
+const BASE_URL = "http://194.62.1.252:5000/api"
 const SIGN_IN = "/auth/signin"
 const USER = "/user"
 const ALL_PRODUCTS = "/products"
